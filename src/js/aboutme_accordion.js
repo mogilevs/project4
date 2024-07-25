f511a21204454b111c7cd90ca3f3389747ac794f
Victoria <vika96karnauhova@gmail.com>
2024-07-25 23:36:49 +0300

    aboutme_accordion

---
+++ src/js/aboutme_accordion.js
@@ -0,0 +1,30 @@
+const titles = document.querySelectorAll('.accordion_title');
+const contents = document.querySelectorAll('.accordion_content');
+
+titles.forEach(item =>
+  item.addEventListener('click', () => {
+    const activeContent = document.querySelector('#' + item.dataset.tab);
+
+    if (activeContent.classList.contains('active')) {
+      activeContent.classList.remove('active');
+      item.classList.remove('active');
+      activeContent.style.maxHeight = 0;
+    } else {
+      contents.forEach(element => {
+        element.classList.remove('active');
+        element.style.maxHeight = 0;
+      });
+
+      titles.forEach(element => element.classList.remove('active'));
+
+      item.classList.add('active');
+      activeContent.classList.add('active');
+      activeContent.style.maxHeight = activeContent.scrollHeight + 'px';
+    }
+  })
+);
+
+document.querySelector('[data-tab="tab-1"]').classList.add('active');
+document.querySelector('#tab-1').classList.add('active');
+document.querySelector('#tab-1').style.maxHeight =
+  document.querySelector('#tab-1').scrollHeight + 'px';
