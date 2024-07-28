3fd6e4dc613740039ac0e03832cb9d69c850b21e
AlinaChorna7 <159713721+AlinaChorna7@users.noreply.github.com>
2024-07-28 22:06:22 +0200

    update2

--- src/main.js
+++ src/main.js
@@ -1,37 +1,145 @@
 
+// import Swiper from 'swiper';
+
+// const swiper = new Swiper('.swiper', {
+//   direction: 'horizontal',
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+//   mousewheel: {
+//     invert: true,
+//   },
+//   keyboard: {
+//     enabled: true,
+//     onlyInViewport: false,
+//   },
+// });
+// const prevButton = document.querySelector('.swiper-button-prev');
+// const nextButton = document.querySelector('.swiper-button-next');
+
+// nextButton.addEventListener('click', moveNext);
+// prevButton.addEventListener('click', movePrev);
+// document.addEventListener('keydown', keyMove);
+
+// function keyMove(evt) {
+//   if (evt.code === 'ArrowRight') {
+//     moveNext();
+//   }
+//   if (evt.code === 'ArrowLeft') {
+//     movePrev();
+//   }
+// }
+
+// function moveNext() {
+//   swiper.slideNext();
+//   updateNavigationButtons();
+// }
+// function movePrev() {
+//   swiper.slidePrev();
+//   updateNavigationButtons();
+// }
+// function updateNavigationButtons() {
+//   if (swiper.isEnd) {
+//     nextButton.classList.add('swiper-button-disabled');
+//     swiper.allowSlideNext = false;
+//   } else {
+//     nextButton.classList.remove('swiper-button-disabled');
+//     swiper.allowSlideNext = true;
+//   }
+
+//   if (swiper.isBeginning) {
+//     prevButton.classList.add('swiper-button-disabled');
+//     swiper.allowSlidePrev = false;
+//   } else {
+//     prevButton.classList.remove('swiper-button-disabled');
+//     swiper.allowSlidePrev = true;
+//   }
+// }
 
 import Swiper from 'swiper';
 
-document.addEventListener('DOMContentLoaded', () => {
-    const swiper = new Swiper('.swiper', {
-        direction: 'horizontal',
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-    });
-
-    function updateNavigationButtons() {
-      const prevButton = document.querySelector('.swiper-button-prev');
-        const nextButton = document.querySelector('.swiper-button-next');
-      
-
-        if (swiper.isEnd) {
-            nextButton.classList.add('swiper-button-disabled');
-            swiper.allowSlideNext = false;
-        } else {
-            nextButton.classList.remove('swiper-button-disabled');
-            swiper.allowSlideNext = true;
-        }
-
-        if (swiper.isBeginning) {
-            prevButton.classList.add('swiper-button-disabled');
-            swiper.allowSlidePrev = false;
-        } else {
-            prevButton.classList.remove('swiper-button-disabled');
-            swiper.allowSlidePrev = true;
-        }
-    }
-
-    swiper.on('slideChange', updateNavigationButtons);
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  mousewheel: {
+    invert: true,
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
 });
+
+
+const prevButton = document.querySelector('.swiper-button-prev');
+const nextButton = document.querySelector('.swiper-button-next');
+const svgNext = document.querySelector('.turn-right');
+const svgPrev = document.querySelector('.turn-left');
+
+
+nextButton.addEventListener('click', moveNext);
+prevButton.addEventListener('click', movePrev);
+document.addEventListener('keydown', keyMove);
+
+
+document.addEventListener('wheel', (evt) => {
+  if (evt.deltaY > 0) {
+    moveNext();
+  } else if (evt.deltaY < 0) {
+    movePrev();
+  }
+});
+
+
+function keyMove(evt) {
+  if (evt.code === 'ArrowRight') {
+    moveNext();
+  }
+  if (evt.code === 'ArrowLeft') {
+    movePrev();
+  }
+}
+
+
+function moveNext() {
+  swiper.slideNext();
+  updateNavigationButtons();
+}
+
+
+function movePrev() {
+  swiper.slidePrev();
+  updateNavigationButtons();
+}
+
+
+function updateNavigationButtons() {
+  if (swiper.isEnd) {
+    nextButton.classList.add('swiper-button-disabled');
+    svgNext.classList.add('swiper-svg-disabled');
+    swiper.allowSlideNext = false;
+  } else {
+    nextButton.classList.remove('swiper-button-disabled');
+    svgNext.classList.remove('swiper-svg-disabled');
+    swiper.allowSlideNext = true;
+  }
+
+  if (swiper.isBeginning) {
+    prevButton.classList.add('swiper-button-disabled');
+svgPrev.classList.add('swiper-svg-disabled');
+    swiper.allowSlidePrev = false;
+  } else {
+    prevButton.classList.remove('swiper-button-disabled');
+    svgPrev.classList.remove('swiper-svg-disabled');
+    swiper.allowSlidePrev = true;
+  }
+
+
+}
+
+
+updateNavigationButtons();
