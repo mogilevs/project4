f6734bdb515fe7b9d110082d69860de4aea93be9
Oleh Mogylevsky <159718895+mogilevs@users.noreply.github.com>
2024-07-30 20:55:02 +0300

    change export inport from footer

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,7 +1,3 @@
-import axios from 'axios';
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
-//==========================================================
 const form = {
   commentsElem: document.getElementById('comments'),
   emailElem: document.getElementById('email'),
@@ -14,59 +10,7 @@ const form = {
 
 const EMAIL_PATTERN = /^\w+(\.\w+)?@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
 const API_URL = 'https://portfolio-js.b.goit.study/api/requests/';
-//=============================================================
 
-// обмеження розміру comments
-form.commentsElem.addEventListener('blur', function () {
-  const maxLength = 10;
-  const textComment = form.commentsElem.value;
-  if (textComment.length > maxLength) {
-    form.commentsElem.value = textComment.substring(0, maxLength) + '...';
-  }
-});
-
-// перевірка на валідність email
-form.emailElem.addEventListener('input', function () {
-  form.successMessage.style.display = 'none';
-  form.errorMessage.style.display = 'none';
-  const email = form.emailElem.value.trim();
-  if (email === '') {
-    form.emailElem.classList.remove('success');
-    form.emailElem.classList.remove('error');
-    return;
-  }
-
-  if (EMAIL_PATTERN.test(email)) {
-    form.emailElem.classList.add('success');
-    form.emailElem.classList.remove('error');
-    form.successMessage.style.display = 'block';
-  } else {
-    form.emailElem.classList.add('error');
-    form.emailElem.classList.remove('success');
-    form.errorMessage.style.display = 'block';
-  }
-});
-
-// збереження данних localStorage
-form.commentsElem.addEventListener('input', function () {
-  localStorage.setItem('comments', form.commentsElem.value);
-});
-
-form.emailElem.addEventListener('input', function () {
-  localStorage.setItem('email', form.emailElem.value);
-});
-
-// загрузка з localStorage
-document.addEventListener('DOMContentLoaded', function () {
-  const savedComments = localStorage.getItem('comments');
-  const savedEmail = localStorage.getItem('email');
-  if (savedComments) {
-    form.commentsElem.value = savedComments;
-  }
-  if (savedEmail) {
-    form.emailElem.value = savedEmail;
-  }
-});
 //==========================================================
 
 // відкривання модального вікна
@@ -81,57 +25,6 @@ function closeModal() {
   document.body.classList.remove('no-scroll');
 }
 //===========================================================
-
-// обробник подій для модал вікна
-form.modalCloseBtn.addEventListener('click', closeModal);
-form.backdrop.addEventListener('click', function (event) {
-  if (event.target === event.currentTarget) {
-    closeModal();
-  }
-});
-document.addEventListener('keydown', function (event) {
-  if (event.key === 'Escape') {
-    closeModal();
-  }
-});
-
-// обробник відпровки форми
-form.formElem.addEventListener('submit', async function (event) {
-  event.preventDefault();
-  const emailValue = form.emailElem.value.trim();
-  const textComment = form.commentsElem.value;
-
-  if (!EMAIL_PATTERN.test(emailValue)) {
-    iziToast.error({
-      title: 'Error:',
-      message: 'Invalid email address',
-    });
-    return;
-  }
-
-  try {
-    const response = await axios.post(API_URL, {
-      email: emailValue,
-      comment: textComment,
-    });
-
-    if (response.status === 201) {
-      openModal();
-      form.successMessage.style.display = 'none';
-      form.emailElem.classList.remove('success');
-      form.formElem.reset();
-      localStorage.removeItem('email');
-      localStorage.removeItem('comments');
-    }
-  } catch (error) {
-    iziToast.error({
-      title: 'Error:',
-      message: 'Error sending request. Try again.',
-    });
-  }
-});
-
-//=============================================================
 
 export { form, EMAIL_PATTERN, API_URL, openModal, closeModal };
 //=============================================================
