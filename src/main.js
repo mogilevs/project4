a87b3411117476bb831a06334b706793da734c8a
Oleh Mogylevsky <159718895+mogilevs@users.noreply.github.com>
2024-08-02 10:33:15 +0300

    import review

--- src/main.js
+++ src/main.js
@@ -10,6 +10,9 @@ import {
   setupSlideChange,
 } from './js/aboutme_3.js';
 import { coversSection, observer } from './js/covers.js';
+
+import { fetchAndDisplayReviews } from './js/review.js';
+
 import {
   form,
   EMAIL_PATTERN,
@@ -19,7 +22,6 @@ import {
 } from './js/footer.js';
 
 import './js/header.js';
-
 import './js/faq';
 import './js/projects.js';
 import './js/aboutme_accordion.js';
@@ -37,6 +39,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
 //================COVERS=============================================
 observer.observe(coversSection);
+
+//================REVIEWS=============================================
+
+document.addEventListener('DOMContentLoaded', fetchAndDisplayReviews);
 
 //================FOOTER=============================================
 
@@ -139,29 +145,3 @@ form.formElem.addEventListener('submit', async function (event) {
     });
   }
 });
-
-
-import {
-  ReviewAPI, toggleLoader, createMarkup, showErrorMessage, showError} from './js/review.js';
-
-document.addEventListener('DOMContentLoaded', fetchAndDisplayReviews);
- 
-async function fetchAndDisplayReviews() {
-    const reviewAPI = new ReviewAPI();
-    toggleLoader(true);
-    try {
-        const data = await reviewAPI.getReviews();
-        if (data && data.length > 0) {
-            createMarkup(data);
-            toggleLoader(false); 
-        } else {
-            toggleLoader(false);
-            showErrorMessage();
-            showError();
-        }
-    } catch (error) {
-        toggleLoader(false);
-        showErrorMessage();
-        showError();
-    }
-}
